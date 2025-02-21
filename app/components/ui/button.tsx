e62cc6a9ee994707d18cba02f63723588d79a49f
wheval <whevalmezeegbe@gmail.com>
2025-02-21 23:04:54 +0100

    add hover state

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -3,22 +3,20 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "relative inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "relative inline-flex items-center rounded-full justify-center font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default: "bg-[#4C327A] text-white hover:bg-opacity-90",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         gradientPrimary: [
-          "p-[1px] bg-gradient-to-b from-[#A26DFF] to-transparent rounded-full hover:bg-red",
-          "shadow-[0px_4px_8px_0px_rgba(194,158,255,0.1)]",
-          "shadow-[-0px_-4px_4px_0px_rgba(162,109,255,0.05)]",
+          "p-[1px] bg-gradient-to-b from-[#A26DFF] to-transparent transition-all rounded-full hover:bg-none",
           "border-transparent",
           "before:absolute before:inset-0 before:rounded-full before:p-[1px] rounded-full",
         ].join(" "),
       },
       size: {
-        default: "h-10",
+        default: "h-12",
       },
     },
     defaultVariants: {
@@ -43,7 +41,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           ref={ref} 
           {...props}
         >
-          <span className="block bg-[#4C327A] rounded-full px-10 py-2 h-full w-full">
+          <span className="bg-[#4C327A] rounded-full flex align-center items-center px-10 py-2 h-full w-full">
             {children}
           </span>
         </button>
