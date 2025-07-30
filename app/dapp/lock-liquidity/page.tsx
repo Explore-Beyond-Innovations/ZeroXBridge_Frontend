8af62ead46a67573ab898bd5cd9a2d76f0675aa1
Ayoazeez26 <ayoazeez26@gmail.com>
2025-07-31 00:36:04 +0100

    feat: made updates to lock tokens page

---
+++ app/dapp/lock-liquidity/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import TokenLockInterface from "@/app/components/lock-tokens";
+import { useTheme } from "@/app/ThemeContext";
+
+const TokenLockPage = () => {
+  const { isDarkMode } = useTheme();
+
+  return (
+    <div
+      className={`flex flex-col items-center justify-center h-fit lg:h-full w-full ${
+        isDarkMode ? "bg-[#09050E]" : "bg-white"
+      }`}>
+      <TokenLockInterface />
+    </div>
+  );
+};
+
+export default TokenLockPage;
