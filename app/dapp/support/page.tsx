975db0cd433ed266ef9815b1ad0acce4118d531c
vaibhav23244 <vv200325803@gmail.com>
2025-08-22 21:02:38 +0530

    Added the reusable faq card component

--- app/dapp/support/page.tsx
+++ app/dapp/support/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import FAQsCard from "@/app/components/dapp/support/FQSCard";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="p-6">
+      <FAQsCard href="/support/faqs" />
+    </div>
+  );
+};
 
-export default page
+export default page;
