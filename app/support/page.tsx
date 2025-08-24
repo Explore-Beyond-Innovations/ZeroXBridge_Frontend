b664add6dc9d2e9b83dbef551c4dc10ca83929cf
best2025j <audugodstime95@gmail.com>
2025-08-24 19:31:45 +0100

    fear: support page guildescard created

---
+++ app/support/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import { GuidesCard } from "./GuidesCard";
+
+export default function SupportPage() {
+  return (
+    <div className="space-y-6 p-6">
+      <h1>Support</h1>
+      <div className="grid-cols-3 w-full gap-6 grid">
+        <GuidesCard />
+      </div>
+    </div>
+  );
+}
