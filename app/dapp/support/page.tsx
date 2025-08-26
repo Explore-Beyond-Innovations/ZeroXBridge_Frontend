608ec11ee23d4dc9659f8557ea92519a0b505442
matthew <ekumamatthew1@gmail.com>
2025-08-26 13:07:19 +0100

    Implement Support Page with Submit Ticket Card Component
    - Created a new SubmitTicketCard component for submitting support tickets.
    - Updated the SupportPage to include the SubmitTicketCard with relevant props.
    - Added an SVG icon for the submit ticket functionality.

--- app/dapp/support/page.tsx
+++ app/dapp/support/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import SubmitTicketCard from "./SubmitTicketCard";
+
+const SupportPage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-col gap-4">
+      <SubmitTicketCard
+        icon="/icons/submit_ticket.svg"
+        title="Submit a Ticket"
+        subtitle="Frequenly Asked Questions"
+      />
+    </div>
+  );
+};
 
-export default page
+export default SupportPage;
